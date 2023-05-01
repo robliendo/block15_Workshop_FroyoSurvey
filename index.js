2642d0be0df15a21be98d33502f03e22a62b7dfc
Ichi Zamboni <ichizamboni@Ichis-MacBook-Pro.local>
2023-05-01 14:37:46 -0700

    adding workshop 15 initial code to repo

--- index.js
+++ index.js
@@ -1,1 +1,142 @@
+// As a test case,  complete the survey using what we know about JavaScript objects.
+// Create an object labeled customer and add the following key:value pairs
 
+// < ----------- FIRST TASK ------------ >
+
+// const customer = {
+//   firstName: "Jake",
+//   lastName: "Smith",
+//   email: "jaekSmih!aol.com",
+//   phone: undefined,
+//   zipCode: "631",
+//   favoriteFlavors: 32,
+//   cupSize: "medium",
+//   favoriteStore: "Target",
+//   firstVisit:	false,
+// };
+
+// console.log(Object.values(customer))
+
+// < ----------- SECOND TASK ------------ >
+
+// Then Modify the following properties in the customer object using bracket notation
+
+// email to "Jak3Smith1992@email.com"
+// phone to 3195551234
+// zipCode to "63132"
+// favoriteFlavors to "coffee", "strawberry", and "matcha
+
+// const customer = {
+//   firstName: "Jake",
+//   lastName: "Smith",
+//   email: "Jak3Smith1992@email.com",
+//   phone: "3195551234",
+//   zipCode: "63132",
+//   favoriteFlavors: ["coffee", "strawberry", "matcha"],
+//   cupSize: "medium",
+//   favoriteStore: "Target",
+//   firstVisit:	false,
+// };
+
+// console.log(Object.values(customer))
+
+// < ----------- THIRD TASK ------------ >
+
+// Then I DELETE the following properties in the customer object using the delete keyword.
+
+// zipCode
+// favoriteStore
+
+// const customer = {
+//   firstName: "Jake",
+//   lastName: "Smith",
+//   email: "Jak3Smith1992@email.com",
+//   phone: "3195551234",
+//   zipCode: "63132",
+//   favoriteFlavors: ["coffee", "strawberry", "matcha"],
+//   cupSize: "medium",
+//   favoriteStore: "Target",
+//   firstVisit:	false,
+// };
+// delete customer ["zipCode"];
+// delete customer ["favoriteStore"]
+
+// console.log(Object.values(customer))
+
+// < ----------- FORTH TASK ------------ >
+
+// NOW! ADD the following items to the survey...
+
+// toppings,
+// future flavors,
+// today's purchase cost,
+
+// And ADD the following key:values to the customer object USING DOT NOTATION:
+
+// toppings:
+// "chocolate sprinkles", "wafer straws", and "gummy bears"
+
+// futureFlavors:
+// "mango"
+
+// todaysPurchaseCost: 5.32
+
+// const customer = {
+//   firstName: "Jake",
+//   lastName: "Smith",
+//   email: "Jak3Smith1992@email.com",
+//   phone: "3195551234",
+//   zipCode: "63132",
+//   favoriteFlavors: ["coffee", "strawberry", "matcha"],
+//   cupSize: "medium",
+//   favoriteStore: "Target",
+//   firstVisit:	false,
+// };
+// delete customer ["zipCode"];
+// delete customer ["favoriteStore"]
+// customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bears"];
+// customer.futureFlavors = "mango";
+// customer.todaysPurchaseCost = "5.32";
+
+// console.table(customer)
+
+// < ----------- FIFTH AND LAST TASK ------------ >
+
+// LAST LAST LAST thing...
+// In an array, print the keys in your survey.
+
+// You should have the following:
+
+// firstName
+// lastName
+// email
+// phone
+// favoriteFlavors
+// cupSize
+// firstVisit
+// toppings
+// futureFlavors
+// todaysPurchaseCost
+
+// const customer = {
+//   firstName: "Jake",
+//   lastName: "Smith",
+//   email: "Jak3Smith1992@email.com",
+//   phone: "3195551234",
+//   zipCode: "63132",
+//   favoriteFlavors: ["coffee", "strawberry", "matcha"],
+//   cupSize: "medium",
+//   favoriteStore: "Target",
+//   firstVisit:	false,
+// };
+// delete customer ["zipCode"];
+// delete customer ["favoriteStore"]
+// customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bears"];
+// customer.futureFlavors = "mango";
+// customer.todaysPurchaseCost = "5.32";
+
+// for(const property in customer){
+//   console.log(property)
+// };
+
+// Thank you.
